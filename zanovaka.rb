7b455e9b72092001b15c2801fe186738cd13a373
Mladen Jablanovic <jablan@radioni.ca>
2017-01-07 10:17:40 +0100

    initial commit

---
+++ zanovaka.rb
@@ -0,0 +1,33 @@
+require 'sinatra'
+require 'csv'
+require 'date'
+
+CONF = {
+  svet: {
+    places: CSV.read('./cities.txt', col_sep: "\t"),
+    zoom: 4..8
+  },
+  srbija: {
+    places: CSV.read('./srbija.txt', col_sep: "\t"),
+    zoom: 9..12
+  }
+}
+COUNTRIES = Hash[CSV.read('./countries.txt', col_sep: "\t")]
+
+get '/' do
+  redirect to('/svet')
+end
+
+get '/:gde' do |gde|
+  @conf = CONF[gde.to_sym]
+  return halt unless @conf
+  @city = @conf[:places].sample
+
+  p COUNTRIES[@city[3]]
+
+  today = Date.today
+  first = Date.new(today.year + 1, 1, 1)
+  @to_go = (first - today).to_i - 1
+
+  erb :index
+end
